e25aef86087d76f944e77bbd05e4ae9bb36c15c6
linga-devops <lingaya.tmst5525@gmail.com@gmail.com>
2023-02-09 18:30:23 -0500

    This is next commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,12 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "===========================" );
+        System.out.println( "This is first java World program" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" ); 
         System.out.println( "Hello World!" ); 
         System.out.println( "Hello World!" );
+        System.out.println( "===========================" );
             
     }
 }
